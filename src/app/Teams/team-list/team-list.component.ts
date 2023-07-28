f7e1e3a594d249b629ac1d6416157308a420db1e
lamzaone <denisdenis009@gmail.com>
2023-07-28 18:40:14 +0300

    Team card now correctly shows (YOU) next to current user

--- src/app/Teams/team-list/team-list.component.ts
+++ src/app/Teams/team-list/team-list.component.ts
@@ -9,8 +9,19 @@ import { User, UserService } from 'src/app/services/user.service';
 export class TeamListComponent {
   
   users: User[];
+  currentUser: User | null; // Property to hold the current user.
 
   constructor(private userService: UserService) {
     this.users = this.userService.getAllUsers();
+    this.currentUser = this.userService.getCurrentUser(); // Implement a method in your UserService to get the current user.
+
+    for (const user of this.users){
+      if (user.id === this.currentUser?.id) {
+        if (user.name.match("(YOU)")) {
+          break;
+        }
+        else user.name += " (YOU)";
+      }
+    }
   }
 }
